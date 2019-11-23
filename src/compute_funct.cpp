12da50c1cd9bca3bb9c066e410e15bd7f819479b
mkara <mert.kara@bilkent.edu.tr>
2019-11-23 23:28:17 +0300

    Partiton output array to allow multiple writes and read, input fifo added

--- src/compute_funct.cpp
+++ src/compute_funct.cpp
@@ -6,9 +6,6 @@
 #include "types.h"
 
 
-
-const int ArrSize = 2048;
-
 static void read_to_BRAM(LineT* inp, ValueT inpBegin, hls::stream<ContribPair> inStream[8], int lineCnt) {
 
 	mem_rd: for (int i=0; i < lineCnt; ++i) {
@@ -18,78 +15,206 @@ static void read_to_BRAM(LineT* inp, ValueT inpBegin, hls::stream<ContribPair> i
 #pragma HLS unroll factor=8 skip_exit_check
 			inStream[j] << line.get(j) ;
 	}
+}
+static void pickAndSend(int streamId,int lineCnt, int ind,int *remLines, ContribPair *pairs, hls::stream<ContribPair>& inStream,hls::stream<ContribPair>& outStream,hls::stream<bool>& validStream){
+#pragma HLS function_instantiate variable=streamId
+	if(*remLines == 0){
+		*pairs = inStream.read();
+		*remLines += 1;
+	}
+	else if( (*remLines < lineCnt+1 )&& ((*pairs).indexData%NUM_OF_PARTITIONS == streamId)){
+		validStream << true;
+		outStream << *pairs;
+		if(*remLines < lineCnt) {
+			*pairs = inStream.read();
+			*remLines += 1;
+		}else *remLines+=5;
+	}
+
 
 }
-static void read_output_to_BRAM(outLineT* inp, int inpBegin, ValueT buffer[BUCKET_WIDTH] ) {
+static void read_input_to_fifos(hls::stream<ContribPair>* inStream, hls::stream<ContribPair>* outStream,hls::stream<bool>* validStream, int lineCnt) {
+	int i;
+	ContribPair pairs[8];
+	int remLines[8];
+	int highestRemLineCount;
+	int highestRemCntIndex;
+	bool done = false;
+#pragma HLS array_partition variable=pairs complete dim=1
+#pragma HLS array_partition variable=remLines complete dim=1
+int li;
+	init_pairs_warm:for(li = 0; li < 8; li++){
+#pragma HLS dependence variable=pairs,inStream intra false
+#pragma HLS dependence variable=pairs,inStream inter false
+#pragma HLS unroll
+		pairs[li] = inStream[li].read();
+		remLines[li] = 1;
+	}
+	while(!done){
+
+/*
+		if( (remLines[li%8] < lineCnt+1 )&& (pairs[li%8].indexData%NUM_OF_PARTITIONS == 0 )){
+			validStream[0] << true;
+			outStream[0] << pairs[li%8];
+			if(remLines[li%8] < lineCnt) {
+				pairs[li%8] = inStream[li%8].read();
+				remLines[li%8]++;
+			}else remLines[li%8]+=5;
+		}
+		if( (remLines[(li+2)%8] < lineCnt+1 )&& (pairs[(li+2)%8].indexData%NUM_OF_PARTITIONS == 1 )){
+			validStream[1] << true;
+			outStream[1] << pairs[(li+2)%8];
+			if(remLines[(li+2)%8] < lineCnt) {
+				pairs[(li+2)%8] = inStream[(li+2)%8].read();
+				remLines[(li+2)%8]++;
+			}else remLines[(li+2)%8]+=5;
+		}
+		if( (remLines[(li+4)%8] < lineCnt+1 )&& (pairs[(li+4)%8].indexData%NUM_OF_PARTITIONS == 2 )){
+			validStream[2] << true;
+			outStream[2] << pairs[(li+4)%8];
+			if(remLines[(li+4)%8] < lineCnt) {
+				pairs[(li+4)%8] = inStream[(li+4)%8].read();
+				remLines[(li+4)%8]++;
+			}else remLines[(li+4)%8]+=5;
+		}
+		if( (remLines[(li+6)%8] < lineCnt+1 )&& (pairs[(li+6)%8].indexData%NUM_OF_PARTITIONS == 3 )){
+			validStream[3] << true;
+			outStream[3] << pairs[(li+6)%8];
+			if(remLines[(li+6)%8] < lineCnt) {
+				pairs[(li+6)%8] = inStream[(li+6)%8].read();
+				remLines[(li+6)%8]++;
+			}else remLines[(li+6)%8]+=5;
+		}
+*/
+//#pragma HLS pipeline
+	stream_unroll: for(int streamId = 0; streamId < NUM_OF_PARTITIONS; streamId++){
+#pragma HLS dependence variable=validStream,outStream,pairs,inStream inter false
+#pragma HLS dependence variable=validStream,outStream,pairs,inStream intra false
+
+#pragma HLS unroll
+		for(int j = streamId*2; j < 8 + streamId*2; j++ ){
+
+				if( (remLines[j%8] < lineCnt+1 )&& (pairs[j%8].indexData%NUM_OF_PARTITIONS == streamId )){
+					validStream[streamId] << true;
+					outStream[streamId] << pairs[j%8];
+					if(remLines[j%8] < lineCnt) {
+						pairs[j%8] = inStream[j%8].read();
+					}
+					remLines[j%8]++;
+				}
+			}
+	}
+	done = ((remLines[0] + remLines[1] +remLines[2]+remLines[3]+remLines[4]+remLines[5]+remLines[6] +remLines[7]) == (lineCnt*8+8));
+	}
+
+	finalize: for(int streamId = 0; streamId < NUM_OF_PARTITIONS; streamId++){
+#pragma HLS unroll
+			validStream[streamId] << false;
+		}
+
+}
+
+static void read_output_to_BRAM(outLineT* inp, int inpBegin, ValueT buffer[NUM_OF_PARTITIONS][BUCKET_WIDTH/NUM_OF_PARTITIONS] ) {
 
 	mem_rd_output: for (int i=0; i < BUCKET_WIDTH/16; ++i) {
 #pragma HLS pipeline II=1
 		outLineT line = inp[inpBegin+i];
 		for(int j = 0; j < 16; j++)
-#pragma HLS unroll factor=2 skip_exit_check
-			buffer[i*16+j] = line.get(j) ;
+			//#pragma HLS unroll factor=NUM_OF_PARTITIONS*2 skip_exit_check
+			buffer[j%NUM_OF_PARTITIONS][(i*16 + j) /NUM_OF_PARTITIONS] = line.get(j) ;
 	}
 }
-static void write_output_to_GMEM(ValueT* inp, int inpBegin, outLineT buffer[BUCKET_WIDTH] ) {
-
-	mem_wr_output: for (int i=0; i < BUCKET_WIDTH; i = i + 16) {
+static void write_output_to_GMEM(ValueT inp[NUM_OF_PARTITIONS][BUCKET_WIDTH/NUM_OF_PARTITIONS], int inpBegin, outLineT buffer[BUCKET_WIDTH/16] ) {
+	outLineT line;
+	ValueT valToWrite;
+	mem_wr_output: for (int i= 0; i < BUCKET_WIDTH; i++) {
 #pragma HLS pipeline II=1
-		outLineT line;
-		for(int j = 0; j < 16; j++)
-{
-			#pragma HLS unroll factor=16 skip_exit_check
-			line.set(j,inp[i+j]);
-			//std::cout << "index: " << i+j << ", val: " << inp[i+j] << std::endl;
+
+		//		for(int j = 0; j < 16; j++)
+		//{
+		//#pragma HLS unroll factor=16 skip_exit_check
+		valToWrite = inp[i%NUM_OF_PARTITIONS][i/NUM_OF_PARTITIONS];
+		line.set(i%16,valToWrite);
+		//}
+		//std::cout << "index1: " << i%NUM_OF_PARTITIONS <<", index2:"<< i/NUM_OF_PARTITIONS << ", val: " << valToWrite << std::endl;
+		if( i%16 == 15) buffer[inpBegin+i/16] = line ;
 	}
-		buffer[inpBegin+i/16] = line ;
-	}
+
 }
 static void read_line_counts(ValueT *in, ValueT* array) {
-lineCount_rd:
-    for (int i = 0; i < NUM_OF_BUCKETS; i++) {
-       #pragma HLS PIPELINE II=1
-        array[i] = in[i];
-    }
+	lineCount_rd:
+	for (int i = 0; i < NUM_OF_BUCKETS; i++) {
+#pragma HLS PIPELINE II=1
+		array[i] = in[i];
+	}
 }
 
-static void compute_kernel(hls::stream<ContribPair> inStream[8], ValueT* arr, int lineCnt ){
+static void compute(hls::stream<ContribPair> &inStream, hls::stream<bool>& validStream ,ValueT* arr ){
+#pragma HLS function_instantiate variable=inStream,validStream
 	IndexT lastAddr = -1;
 	ValueT lastVal = 0;
 	ContribPair pair;
 	IndexT index;
 	ValueT val;
 	ValueT currVal;
-#pragma HLS inline off
+	bool valid;
 
-	comp: for(int i = 0; i < lineCnt*8; i++){
-#pragma HLS pipeline II=1
+	comp:for(valid = validStream.read(); valid != false; valid = validStream.read()){
+
 #pragma HLS dependence variable=arr distance=2 inter true
+#pragma HLS pipeline II=1
 
-		pair = inStream[i%8].read() ; // read the current address
-		index = pair.indexData;
-		val = pair.valData;
+		//std::cout << "Valid: " << valid << ", i: " << i << std::endl;
+		//ContribPair pair;
 
-		if (index == lastAddr) {
+		pair = inStream.read() ; // read the current address
+		index = pair.indexData / NUM_OF_PARTITIONS;     //0 4 8
+		//std::cout << "INdex: " << index << ", i: " << pair.indexData << std::endl;
+		val = pair.valData;			//1 5 9
+		//2 6 10
+		if (index == lastAddr) {	//3 7 11
 			currVal = lastVal ; // pipeline forwarding
 		}
 		else {
-			currVal = arr[index % BUCKET_WIDTH];
+			currVal = arr[index];
 		}
 
-		arr[index % BUCKET_WIDTH] = currVal + val; // update the current value and store it
-		//std::cout << " val: " << lastVal << std::endl;
+		arr[index] = currVal + val; // update the current value and store it
 
 		lastAddr = index ;
 		lastVal = currVal + val ;
 	}
+
 }
-static void read_and_compute(LineT* inp, ValueT inpBegin, ValueT* arr,int lineCnt){
+static void compute_kernel(hls::stream<ContribPair>* inStream,hls::stream<bool>* validStream, ValueT arr[NUM_OF_PARTITIONS][BUCKET_WIDTH/NUM_OF_PARTITIONS], int lineCnt ){
+
+	comp_unroll_loop: for(int i = 0; i < 4; i++){
+		//#pragma HLS array_partition variable=arr cyclic factor=4 dim=1
+
+#pragma HLS dependence variable=arr inter false
+		//#pragma HLS dependence variable=arr intra false
+#pragma HLS unroll
+		compute(inStream[i], validStream[i], arr[i]);
+	}
+}
+static void read_and_compute(LineT* inp, ValueT inpBegin, ValueT arr[NUM_OF_PARTITIONS][BUCKET_WIDTH/NUM_OF_PARTITIONS],int lineCnt){
 	hls::stream<ContribPair> inStream[8];
-	#pragma HLS STREAM variable = inStream depth = 12
-	#pragma HLS array_partition variable=inStream complete
-	#pragma HLS dataflow
+	hls::stream<ContribPair> outStream[4];
+	hls::stream<bool> validStream[4];
+
+#pragma HLS STREAM variable = inStream depth = 32
+#pragma HLS STREAM variable = outStream depth = 32
+#pragma HLS STREAM variable = validStream depth = 32
+
+#pragma HLS array_partition variable=inStream complete
+#pragma HLS array_partition variable=outStream complete
+#pragma HLS array_partition variable=validStream complete
+
+#pragma HLS dataflow
+
 	read_to_BRAM(inp, inpBegin, inStream, lineCnt);
-	compute_kernel(inStream,arr,lineCnt);
+	read_input_to_fifos(inStream, outStream, validStream,lineCnt);
+	compute_kernel(outStream,validStream ,arr,lineCnt);
 }
 extern "C" {
 
@@ -105,21 +230,21 @@ void top_kernel(LineT* inputVals, LineT* indexVals, outLineT* outputSums, ValueT
 #pragma HLS INTERFACE s_axilite port=return bundle=control
 
 
-	ContribPair inputArray[ArrSize];
-	ValueT outputArray[BUCKET_WIDTH];
+	ValueT outputArray[NUM_OF_PARTITIONS][BUCKET_WIDTH/NUM_OF_PARTITIONS]; //manual cyclic partition
 	ValueT lCounts[NUM_OF_BUCKETS];
-	//static hls::stream<ContribPair> inStream[8];
-	//#pragma HLS STREAM variable = inStream depth = 4
 
-	//#pragma HLS array_partition variable=buffers complete dim=1
+
+#pragma HLS array_partition variable=outputArray complete dim=1
+//#pragma HLS array_partition variable=outputArray complete dim=2
+
 	ValueT inputStreamStartingIndex = 0;
 
 	read_line_counts(lineCounts, lCounts);
+
 	for(int i = 0; i < NUM_OF_BUCKETS; i++){
+//#pragma HLS unroll factor=2
 		read_output_to_BRAM(outputSums, i*BUCKET_WIDTH/16, outputArray);
 		read_and_compute(inputVals, inputStreamStartingIndex , outputArray,lCounts[i]);
-//		read_to_BRAM(inputVals, &inputStreamStartingIndex, inStream, lCounts[i]);
-//		compute_kernel(inStream,outputArray,lCounts[i]);
 		write_output_to_GMEM(outputArray, i*BUCKET_WIDTH/16, outputSums);
 		inputStreamStartingIndex += lCounts[i];
 	}
--- src/types.h
+++ src/types.h
@@ -9,8 +9,9 @@ using std::vector;
 using std::endl;
 using std::string ;
 //PARAM DEFINITIONS
+#define NUM_OF_PARTITIONS 4
 #define NDDR_BANKS 3
-#define OUTPUT_VECTOR_SIZE 1024*5 // 1 MB
+#define OUTPUT_VECTOR_SIZE 1024*15 // 1 MB
 #define BUCKET_WIDTH 512 //in terms of elements
 #define OUTPUT_LINE_SIZE 16
 #define INPUT_LINE_SIZE 8 //Contrib Pairs
@@ -21,8 +22,8 @@ using std::string ;
 #define NUM_OF_BUCKETS OUTPUT_VECTOR_SIZE/BUCKET_WIDTH // assume divisible for now
 
 //bucket densities for testing
-#define BUCKET_DENSITY_FOR_TESTING 2
-#define NUM_OF_TEST_SIZE 2048*8
+#define BUCKET_DENSITY_FOR_TESTING 3
+//#define NUM_OF_TEST_SIZE 2048*8
 
 #define BURST_LINE_CNT 32
 #define VECTOR_SIZE 8
@@ -81,7 +82,7 @@ struct Type512 {
 	ap_uint<512> data;
   ContribPair get(int idx) {
     // HLS INLINE pragma is important. The tool does not recognize the C++ "inline" keyword.
-#pragma HLS INLINE off
+#pragma HLS INLINE
 	  ContribPair res;
 	  ap_uint<64> pair = data((idx+1)*64-1,idx*64);
 	  res.indexData = pair(31,0);
@@ -112,13 +113,13 @@ struct Type512_output {
   ap_uint<512> data ;
   ValueT get(int idx) {
     // HLS INLINE pragma is important. The tool does not recognize the C++ "inline" keyword.
-#pragma HLS INLINE off
+#pragma HLS INLINE
     return data((idx+1)*BitCnt<ValueT>::get()-1,idx*BitCnt<ValueT>::get()) ;
   }
 
   void set(int idx, ValueT val) {
     // HLS INLINE pragma is important. The tool does not recognize the C++ "inline" keyword.
-#pragma HLS INLINE off
+#pragma HLS INLINE
     data((idx+1)*BitCnt<ValueT>::get()-1, idx*BitCnt<ValueT>::get()) = val ;
   }
 
